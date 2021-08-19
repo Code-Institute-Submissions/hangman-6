693a5ef7f83ba31afe1dbd96f92372bb142ec3a3
gorsig <sigeskog@hotmail.se>
2021-08-19 08:48:39 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import json
+import sys
+import operator
+
+word_list = [
+        'wares',
+        'soup',
+        'mount',
+        'extend',
+        'brown',
+        'computer',
+        'apple',
+        'market',
+        'school',
+        'coin',
+        'money',
+        'dog',
+        'transform',
+        'collect',
+        'party',
+        'friends',
+        'cool',
+        'banana',
+        'cat',
+        'animal',
+        'justice',
+        'breakdance',
+        'happy'
+    ]
+player = {'GORAN': 0}
+
